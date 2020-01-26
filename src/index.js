ca2bf75a3a37f725bb5662ee60415b822bc1f74c
gilbertomarcano <marcanogilbertoa@gmail.com>
2020-01-26 18:14:47 -0400

    Adding API

--- src/index.js
+++ src/index.js
@@ -9,9 +9,45 @@ function showVersions() {
 const electron = require('electron')
 const path = require('path')
 const BrowserWindow = electron.remote.BrowserWindow
-const {dialog} = require('electron')
 
 const notifyBtn = document.getElementById('notifyBtn')
+var price = document.querySelector('h1')
+var targetPrice = document.getElementById('targetPrice')
+
+function getUSD(url, success) {
+
+    var ud = '_' + +new Date,
+        script = document.createElement('script'),
+        head = document.getElementsByTagName('head')[0] 
+               || document.documentElement;
+
+    window[ud] = function(data) {
+        head.removeChild(script)
+        success && success(data)
+    }
+
+    script.src = url.replace('callback=?', 'callback=' + ud);
+    head.appendChild(script);
+}
+
+function getAPI(url) {
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET", url, false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
+}
+
+function getUSD() {
+    api = JSON.parse(getAPI('https://s3.amazonaws.com/dolartoday/data.json'))
+    console.log(api)
+    console.log(api.USD.transferencia)
+    var current_price = api.USD.transferencia
+    price.innerHTML = 'Bs. ' + current_price.toLocaleString('en')
+}
+
+getUSD()
+setInterval(getUSD, 30000)
+
 
 notifyBtn.addEventListener('click', function(event) {
     const modalPath = path.join('file://', __dirname, 'add.html')
