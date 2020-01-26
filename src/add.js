0fd35952891e510e696dedefbef9aca8b8d6e9d4
gilbertomarcano <marcanogilbertoa@gmail.com>
2020-01-25 20:53:50 -0400

    Adding windows

---
+++ src/add.js
@@ -0,0 +1,20 @@
+// const electron = require('electron')
+//const path = require('path')
+//const remote = electron.remote
+
+// <script type="text/javascript" src="add.js"></script>
+
+closeBtn = document.getElementById('closeBtn')
+
+closeBtn.addEventListener('click', function(event) {
+    close()
+})
+
+//const closeBtn = document.getElementById('closeBtn')
+//alert('aaaaaaaaaa')
+
+// closeBtn.addEventListener('click', function(event) {
+//     var window = electron.remote.getCurrentWindow()
+//     //var window = remote.getCurrentWindow()
+//     window.close()
+// })
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-function showVersion() {
+function showVersions() {
     document.write(
         'We are using node ' + process.versions.node +
-        'Chrome ' + process.versions.chrome  +
-        'and Electron ' + process.versions.electron
+        ', Chrome ' + process.versions.chrome  +
+        ' and Electron ' + process.versions.electron
     )
 }
+
+const electron = require('electron')
+const path = require('path')
+const BrowserWindow = electron.remote.BrowserWindow
+const {dialog} = require('electron')
+
+const notifyBtn = document.getElementById('notifyBtn')
+
+notifyBtn.addEventListener('click', function(event) {
+    const modalPath = path.join('file://', __dirname, 'add.html')
+    let win = new BrowserWindow({ frame: false, transparent: true, alwaysOnTop: true, width: 400, height: 200})
+    win.on('close', function() { win = null })
+    win.loadURL(modalPath)
+    win.show()
+})
