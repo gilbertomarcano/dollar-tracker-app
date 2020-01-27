5daa1776cf7905b8a01e401502d2fc0fdbec7440
gilbertomarcano <marcanogilbertoa@gmail.com>
2020-01-26 20:15:21 -0400

    Adding the Inter Process Communication (IPC)

--- src/add.js
+++ src/add.js
@@ -1,5 +1,17 @@
 closeBtn = document.getElementById('closeBtn')
 
+alert('add.js')
+
 closeBtn.addEventListener('click', function(event) {
+    close()
+})
+
+
+const ipc = require('electron').ipcRenderer
+
+const updateBtn = document.getElementById('updateBtn')
+
+updateBtn.addEventListener('click', function() {
+    ipc.send('update-notify-value', document.getElementById('notifyVal').value)
     close()
 })
--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ function showVersions() {
 
 const electron = require('electron')
 const path = require('path')
-const BrowserWindow = electron.remote.BrowserWindow
+const { BrowserWindow } = require('electron').remote
+const ipc = require('electron').ipcRenderer
 
 const notifyBtn = document.getElementById('notifyBtn')
 var price = document.querySelector('h1')
@@ -51,8 +52,24 @@ setInterval(getUSD, 30000)
 
 notifyBtn.addEventListener('click', function(event) {
     const modalPath = path.join('file://', __dirname, 'add.html')
-    let win = new BrowserWindow({ frame: false, transparent: true, alwaysOnTop: true, width: 400, height: 200})
-    win.on('close', function() { win = null })
+    let win = new BrowserWindow({
+        frame: false,
+        transparent: true, 
+        alwaysOnTop: true, 
+        width: 900, 
+        height: 650,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    win.on('close', () => { 
+        win = null 
+    })
     win.loadURL(modalPath)
     win.show()
+})
+
+ipc.on('targetPriceVal', function (event, arg) {
+    targetPriceVal = Number(arg)
+    targetPrice.innerHTML = 'Bs. ' + targetPriceVal.toLocaleString('en')
 })
